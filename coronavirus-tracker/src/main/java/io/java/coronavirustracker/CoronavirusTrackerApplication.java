d4348da997269c4bffbf5e145206493696a4a6b2
Safal Adhikari <safal.adhikari1@outlook.com>
2020-08-29 23:20:40 -0700

    adds CoronaVirusDataService class, that retrieves and parses live CSV data using external library

--- coronavirus-tracker/src/main/java/io/java/coronavirustracker/CoronavirusTrackerApplication.java
+++ coronavirus-tracker/src/main/java/io/java/coronavirustracker/CoronavirusTrackerApplication.java
@@ -2,11 +2,15 @@ package io.java.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class CoronavirusTrackerApplication {
+@EnableScheduling
+public class CoronavirusTrackerApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(CoronavirusTrackerApplication.class, args);
 	}
 
